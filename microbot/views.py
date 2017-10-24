bb9137be2ab1e57a647d57c83269c18be5e48468
MIKE <rlghd1996@gmail.com>
2017-10-24 20:34:30 +0900

    add geckodriver

--- microbot/views.py
+++ microbot/views.py
@@ -1,7 +1,17 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from Crawling.Crawl import Crawl
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def crawl(request):
+        req = ((request.body).decode('utf-8'))
+        return_json_str = json.loads(req)
+        mid = return_json_str['mid']
+
+        obj = Crawl()
 
         return JsonResponse({
                 'type' : 'buttons',
